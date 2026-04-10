aa72bece7675c0bdc30b4fbb4f0a6125a5232cc4
Gerson Focus delantero <gfuentesfwd@gmail.com>
2026-04-10 14:15:36 -0600

    cambie packagejson

--- controladores/EmpleadoControlador.js
+++ controladores/EmpleadoControlador.js
@@ -1,22 +1,22 @@
 import Empleado from "../modelos/EmpleadoModelos.js";
-import { guardar_nuevo_empleado } from "../conector.js"; // 
+import { guardar_nuevo_empleado } from "../conector.js";
 
+// 🟢 Exportamos la función directamente
 export const registrarNuevoColaborador = async (req, res) => {
     try {
         const { id, nombre, correo, puesto, salario } = req.body;
 
-        // 🟢 Creamos la instancia con tu clase POO (¡Herencia al poder!)
+        // Uso de la herencia de tus clases POO
         const nuevoEmpleado = new Empleado(id, nombre, correo, puesto, salario);
 
-        // 🟢 Guardamos en el JSON Server a través del conector
+        // Guardamos en el archivo JSON
         const resultado = await guardar_nuevo_empleado(nuevoEmpleado);
 
         res.json({
-            mensaje: " trabajador puesto",
-            datos: resultado
+            mensaje: "✅ Datos recibidos y guardados en el JSON",
+            empleado_creado: resultado
         });
-        
     } catch (error) {
-        res.status(500).json({ mensaje: "Error al guardar", detalle: error.message });
+        res.status(500).json({ mensaje: "Error en el servidor", detalle: error.message });
     }
 };
