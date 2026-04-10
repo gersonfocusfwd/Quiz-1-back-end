479b6851302877b672acc4b1b35d6789559b9a80
Gerson Focus delantero <gfuentesfwd@gmail.com>
2026-04-10 14:05:21 -0600

    agregue nuevas cosas o actualice archivo de empleadocontrolador

--- controladores/EmpleadoControlador.js
+++ controladores/EmpleadoControlador.js
@@ -1,19 +1,22 @@
 import Empleado from "../modelos/EmpleadoModelos.js";
+import { guardar_nuevo_empleado } from "../conector.js"; // 
 
-const registrarNuevoColaborador = (req, res) => {
-    const { id, nombre, correo, puesto, salario } = req.body;
+export const registrarNuevoColaborador = async (req, res) => {
+    try {
+        const { id, nombre, correo, puesto, salario } = req.body;
 
-    
-    // uso de la herencia
-    const nuevoEmpleado = new Empleado(id, nombre, correo, puesto, salario);
+        // 🟢 Creamos la instancia con tu clase POO (¡Herencia al poder!)
+        const nuevoEmpleado = new Empleado(id, nombre, correo, puesto, salario);
 
-    res.json({
-        mensaje: "¡Datos recibidos en el Body del API!",
-        empleado: nuevoEmpleado,
-        confirmacion: nuevoEmpleado.obtenerDetalles()
-    });
-};
+        // 🟢 Guardamos en el JSON Server a través del conector
+        const resultado = await guardar_nuevo_empleado(nuevoEmpleado);
 
-export const EmpleadoControlador = {
-    registrarNuevoColaborador
+        res.json({
+            mensaje: " trabajador puesto",
+            datos: resultado
+        });
+        
+    } catch (error) {
+        res.status(500).json({ mensaje: "Error al guardar", detalle: error.message });
+    }
 };
